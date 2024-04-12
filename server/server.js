9608a5ab5c28fa61bb346a7a28b9d5a450e5f441
Sarah Wiseman <s.wiseman777@gmail.com>
2024-04-12 14:10:59 +0100

    more render url edits...

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import cors from "cors"; // import cors from the CORS module that was installed
 
 const db = new Database("database.db");
 const app = express(); // creating an app using the express object
-const PORT = 5000; // specifying the port number
+// const PORT = 5000; // specifying the port number
 app.use(express.json()); // accept json in requests
 app.use(cors()); // allows client to communicate with the server without it being blocked
 
@@ -79,6 +79,8 @@ app.post("/getmessage", (req, res) => {
 //   res.json({ status: "Message received!" }); // this is seen in postman when we do a POST request on localhost:5000/message - we will only see GET requests on the webpage
 // });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: http://localhost:${PORT}`);
+app.listen("https://tech-ed-project-04-1.onrender.com", () => {
+  console.log(
+    `Server is running on port: https://tech-ed-project-04-1.onrender.com`
+  );
 });
