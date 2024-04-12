22d73650504202f0b1778076994c1b935ea20f26
Sarah Wiseman <s.wiseman777@gmail.com>
2024-04-12 14:33:00 +0100

    please work on render

--- server/server.js
+++ server/server.js
@@ -79,8 +79,6 @@ app.post("/getmessage", (req, res) => {
 //   res.json({ status: "Message received!" }); // this is seen in postman when we do a POST request on localhost:5000/message - we will only see GET requests on the webpage
 // });
 
-app.listen("https://tech-ed-project-04-1.onrender.com/", () => {
-  console.log(
-    `Server is running on port: https://tech-ed-project-04-1.onrender.com/`
-  );
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
